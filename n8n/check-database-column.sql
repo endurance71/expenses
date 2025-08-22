4c48135c5b8b9f67c0843b05a48667672d3b28ac
endurance71 <80155144+endurance71@users.noreply.github.com>
2025-08-22 16:57:53 +0200

    2.0

--- n8n/check-database-column.sql
+++
@@ -1,9 +0,0 @@
--- Sprawdź czy kolumna receipt_image_url istnieje
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns 
-WHERE table_name = 'detailed_expenses'
-AND column_name = 'receipt_image_url';
-
--- Jeśli zwróci 0 wierszy, uruchom:
-ALTER TABLE detailed_expenses 
-ADD COLUMN receipt_image_url TEXT;
